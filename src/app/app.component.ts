f6af2ff03e1aeef21b4d089eaa97b23959723f6c
valeroMC <valerocomingsoon@gmail.com>
2021-05-11 21:04:07 -0500

    Creatting angular form with crud.

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "Formulario"; 
+}
+function goToLink (event, url) {
+  var type = event.type;
+
+  if (
+    (type === 'click') ||
+    (type === 'keydown' && event.keyCode === 13)
+  ) {
+    window.location.href = url;
+
+    event.preventDefault();
+    event.stopPropagation();
+  }
+}
